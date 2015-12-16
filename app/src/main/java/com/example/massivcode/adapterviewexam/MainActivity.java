c200b2b9d061d6ec8c8e65be426a0bd1709b7440
prchoe <massivcode@gmail.com>
2015-12-16 19:37:25 +0900

    init 2

---
+++ app/src/main/java/com/example/massivcode/adapterviewexam/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.massivcode.adapterviewexam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 리스트 뷰의 어댑터에 담길 데이터 입니다.
+    private List<Unit> mData;
+
+    // 담길 데이터를 초기화하는 메소드 입니다.
+    private void initData() {
+        mData = new ArrayList<>();
+
+        mData.add(new Unit("테란", "마린"));
+        mData.add(new Unit("테란", "메딕"));
+        mData.add(new Unit("프로토스", "질럿"));
+        mData.add(new Unit("프로토스", "프로브"));
+        mData.add(new Unit("저그", "라바"));
+        mData.add(new Unit("저그", "저글링"));
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+    }
+}
